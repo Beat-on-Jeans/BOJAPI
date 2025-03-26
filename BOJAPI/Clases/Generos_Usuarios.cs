6f0fa08b5e126803a6199abf22036355b27ef920
Helio Verdugo <hverdugob2425@politecnics.barcelona>
2025-03-26 18:02:19 +0100

    Primer Intento devolver generos musicales

---
+++ BOJAPI/Clases/Generos_Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BOJAPI.Clases
+{
+    public class Generos_Usuarios
+    {
+        public int ID { get; set; }
+
+        public int Genero_Id { get; set; }
+
+        public int Usuario_Id { get; set; }
+
+        public virtual ICollection<GenerosMusicales> Generos { get; set; }
+    }
+}
